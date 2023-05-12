ceda231f722d3ae1879988e9f91eb5d806f7453b
jspinad <johanpina1420@gmail.com>
2023-05-11 23:44:56 -0500

    cambiamos los tags para la documentación, actualizamos el readme con las dependencias de instalación y de configuración y montaje de la carpeta en red

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from fastapi.staticfiles import StaticFiles
 from schemas import HTTPError, OcrResult, ZerSchema
 from ocr import ocr_image, ocr_ZER
 
-app = FastAPI()
+app = FastAPI(title='OCR PeopleContact')
 app.mount("/static", StaticFiles(directory="static/"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse,tags=["WebInterface"])
 async def get_root(request: Request):
     with open("static/index.html", "r") as f:
         html_content = f.read()
     return html_content
 
-@app.post("/img2text/",response_model=OcrResult)
+@app.post("/img2text/",response_model=OcrResult,tags=["OCR4all"])
 async def upload_image(file: UploadFile = File(...)):
     image_data = await file.read()
     text = await ocr_image(image_data)
@@ -25,8 +25,8 @@ async def upload_image(file: UploadFile = File(...)):
 @app.post("/InfoPagoZer/",response_model=ZerSchema,responses={
                                                             200:{"model":ZerSchema},
                                                             409:{"model":HTTPError, "description":"Error Saving Image in SharedFolder"}
-                                                            }# Este responses es para las excepciones en la documentación. Si se hace así queda la documentación.   
-          )
+                                                            },# Este responses es para las excepciones en la documentación. Si se hace así queda la documentación.   
+          tags=["ZERocr"])
 
 async def upload_image(file: UploadFile = File(...)):
     image_data = await file.read()
